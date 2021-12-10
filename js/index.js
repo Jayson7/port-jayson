acb5d835978172e7c829d9cc94257b79a16f47ad
jayson7 <lexxiijoo70@gmail.com>
2021-12-10 11:53:03 +0100

    frontend

--- js/index.js
+++ js/index.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
         $("header").addClass("active-header");
         $("header").removeClass("header-wraps");
     });
+    $(".fa-times").click(function() {
+        $("header").addClass("header-wraps");
+        $("header").removeClass("active-header");
+    });
 });
